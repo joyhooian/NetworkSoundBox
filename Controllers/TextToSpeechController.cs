852aef15796ca3be3ed13242cdc2fbfc076754d0
JaseIan <joyhooian@hotmail.com>
2021-10-25 00:28:07 +0800

    -

--- Controllers/TextToSpeechController.cs
+++ Controllers/TextToSpeechController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NetworkSoundBox.Services.TextToSpeech;
+using NetworkSoundBox.Controllers.DTO;
+using Newtonsoft.Json;
 
 namespace NetworkSoundBox.Controllers
 {
@@ -13,10 +15,12 @@ namespace NetworkSoundBox.Controllers
     public class TextToSpeechController : ControllerBase
     {
         private readonly IXunfeiTtsService _xunfeiTtsService;
+        private readonly IDeviceSvrService _deviceService;
 
-        public TextToSpeechController(IXunfeiTtsService xunfeiTtsService)
+        public TextToSpeechController(IXunfeiTtsService xunfeiTtsService, IDeviceSvrService deviceSvrService)
         {
             _xunfeiTtsService = xunfeiTtsService;
+            _deviceService = deviceSvrService;
         }
 
         [HttpGet("BluetoothPlayerTTS/TTS/{text}")]
@@ -31,6 +35,36 @@ namespace NetworkSoundBox.Controllers
         {
             var speech = await _xunfeiTtsService.GetSpeech(text, vcn.ToString().ToLower(), speed, volumn, pitch);
             return new FileContentResult(speech.ToArray(), "audio/mpeg");
+        }
+
+        [HttpGet("DownloadTTS/sn{sn}/text{text}/vcn{vcn}/speed{speed}/volumn{volumn}")]
+        public async Task<string> DownloadTTS(string sn, string text, VCN vcn = VCN.XIAOYAN, int speed = 50, int volumn = 50)
+        {
+            DeviceHandler device = null;
+            try
+            {
+                device = _deviceService.DevicePool.First(device => device.SN == sn);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            if (device == null)
+            {
+                return JsonConvert.SerializeObject(new FileResultDto("fail", "Device is disconnected."));
+            }
+            var speech = await _xunfeiTtsService.GetSpeech(text, vcn.ToString().ToLower(), speed, volumn, 50);
+            var fileUploaded = new File(speech);
+            device.FileQueue.Add(fileUploaded);
+            fileUploaded.Semaphore.WaitOne();
+            if (fileUploaded.FileStatus == FileStatus.Success)
+            {
+                return JsonConvert.SerializeObject(new FileResultDto("success", ""));
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(new FileResultDto("fail", "Transmit file failed."));
+            }
         }
     }
 
