19f9a2f72985d3c215abe40ce0dda311918a237a
JaseIan <joyhooian@hotmail.com>
2022-01-15 16:43:53 +0800

    -

---
+++ Nsb.Type/DeviceType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsb.Type
+{
+    public enum DeviceType
+    {
+        WiFi_Test = 1,
+        Cellular_Test = 0x11
+    }
+}
---
+++ Nsb.Type/PermissionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsb.Type
+{
+    public enum PermissionType
+    {
+        Admin = 1,
+        Control,
+        View
+    }
+}
