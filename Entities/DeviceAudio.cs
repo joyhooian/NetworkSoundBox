64f442072babe7c1c5d07d46e468ef0100ae06ae
JaseIan <joyhooian@hotmail.com>
2022-04-12 23:55:05 +0800

    merge from master

---
+++ Entities/DeviceAudio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NetworkSoundBox.Entities
+{
+    public partial class DeviceAudio
+    {
+        public int DeviceAudioKey { get; set; }
+        public string DeviceReferenceId { get; set; }
+        public string AudioReferenceId { get; set; }
+        public int Index { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
